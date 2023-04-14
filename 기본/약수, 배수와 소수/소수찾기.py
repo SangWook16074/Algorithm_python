6ce279c6e7fcc29cf3505b04f986877784aaa529
HanSangWook <qazws78941@gmail.com>
2023-04-14 09:18:06 +0900

    소수 찾기 해결

---
+++ 기본/약수, 배수와 소수/소수찾기.py
@@ -0,0 +1,31 @@
+# 문제
+# 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+# 출력
+# 주어진 수들 중 소수의 개수를 출력한다.
+
+# 예제 입력 1 
+# 4
+# 1 3 5 7
+# 예제 출력 1 
+# 3
+
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+cnt = 0
+
+numbers = list(map(int, sys.stdin.readline().split()))
+for num in numbers:
+    tmp = 0
+    for i in range(1, num+1):
+        if num % i == 0:
+            tmp += 1
+        
+    if tmp == 2:
+        cnt += 1
+
+print(cnt)
