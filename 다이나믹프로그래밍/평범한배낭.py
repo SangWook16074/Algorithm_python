6c57125989c00ab535b1eff7cc30d164d2dd4753
HanSangWook <qazws78941@gmail.com>
2023-08-22 09:37:15 +0900

    배낭과 01타일 해결

--- 다이나믹프로그래밍/평범한배낭.py
+++ 다이나믹프로그래밍/평범한배낭.py
@@ -33,10 +33,19 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-arr = []
+arr = [[0,0]]
 for _ in range(n):
     w, v = map(int, input().split())
     arr.append([w, v])
 
-d = [0]*10001
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
 
+for i in range(1, n+1):
+    for w in range(1, k+1):
+        weight = arr[i][0]
+        value = arr[i][1]
+        if w < weight:
+            dp[i][w] = dp[i-1][w]
+        else:
+            dp[i][w] = max(dp[i-1][w-weight]+value, dp[i-1][w])
+print(dp[n][k])
