b7aba87410be06749bd6cf673a642da0de78f46e
HanSangWook <qazws78941@gmail.com>
2023-09-06 19:41:32 +0900

    기타레슨 해결

--- 이진탐색/기타레슨.py
+++ 이진탐색/기타레슨.py
@@ -6,22 +6,20 @@ lectures = list(map(int, input().split()))
 
 start = max(lectures)
 end = start * n
-if m == n:
-    print(max(lectures))
-else:
-    while start <= end:
-        mid = (start + end) // 2
-        cnt = 1
-        length = mid
-        for l in lectures:
-            if length < l:
-                cnt += 1
-                length = mid
-            length -= l
 
-        if cnt <= m:
-            result = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    print(result)
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 1
+    length = mid
+    for l in lectures:
+        if length < l:
+            cnt += 1
+            length = mid
+        length -= l
+
+    if cnt <= m:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+print(result)
