11a92fcc316bde9cc3eb41e05a8db2266860774f
HanSangWook <qazws78941@gmail.com>
2023-09-28 18:34:21 +0900

    카드합체놀이 해결

--- 우선순위큐/카드합체놀이.py
+++ 우선순위큐/카드합체놀이.py
@@ -7,9 +7,7 @@ n, m = map(int, input().split())
 h = []
 for card in list(map(int, input().split())):
     heapq.heappush(h, card)
-
 for _ in range(m):
-    # 카드 선택
     total = heapq.heappop(h) + heapq.heappop(h)
     heapq.heappush(h, total)
     heapq.heappush(h, total)
