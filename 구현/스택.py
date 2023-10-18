23ad97713644f14b520740273c96d6d5df04ac42
HanSangWook <qazws78941@gmail.com>
2023-10-18 09:46:19 +0900

    스택 해결

---
+++ 구현/스택.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+for _ in range(n):
+    command = input().split()
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'top':
+        print(stack[-1] if len(stack) > 0 else -1)
+    elif command[0] == 'pop':
+        print(stack.pop() if len(stack) > 0 else -1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        print(0 if len(stack) > 0 else 1)
