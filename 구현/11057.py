a0f95aa134c573a5fafb0260ac9ede2a5375b403
HanSangWook <qazws78941@gmail.com>
2024-01-15 10:17:29 +0900

    11057 문제 해결

---
+++ 구현/11057.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[1 for _ in range(10)] for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, 10):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+print(dp[n][9] % 10007)
