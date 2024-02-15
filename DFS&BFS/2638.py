7dcd2a5524f4000b74553d492cbce68b79d56788
HanSangWook <qazws78941@gmail.com>
2024-02-15 09:36:06 +0900

    시간복잡도 개선

--- DFS&BFS/2638.py
+++ DFS&BFS/2638.py
@@ -1,26 +1,37 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
+
+
+def check(x, y):
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            return True
+    return False
 
 
 def bfs(x, y):
     visited = [[False for _ in range(m)] for _ in range(n)]
     q = deque()
-    q.append((x, y))
+    ex.append((x, y))
     visited[x][y] = True
+    graph[x][y] = 2
+    q.append((x, y))
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                return True
-
+                continue
             if not visited[nx][ny] and graph[nx][ny] == 0:
                 visited[nx][ny] = True
+                graph[nx][ny] = 2
                 q.append((nx, ny))
-    return False
+                ex.append((nx, ny))
 
 
 n, m = map(int, input().split())
@@ -28,25 +39,31 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 result = 0
 while True:
     flag = False
-    tmp = []
+    ex = []
+    for x in range(n):
+        for y in range(m):
+            if graph[x][y] == 0 and check(x, y):
+                bfs(x, y)
+
+            if graph[x][y] == 1:
+                flag = True
+    if not flag:
+        break
+
     for x in range(n):
         for y in range(m):
             if graph[x][y] == 1:
-                flag = True
                 cnt = 0
                 for i in range(4):
                     nx, ny = x + dx[i], y + dy[i]
-                    if graph[nx][ny] == 0 and bfs(nx, ny):
+                    if graph[nx][ny] == 2:
                         cnt += 1
-                        if cnt >= 2:
-                            tmp.append((x, y))
+                        if cnt == 2:
+                            graph[x][y] = 0
                             break
-    for x, y in tmp:
+
+    for x, y in ex:
         graph[x][y] = 0
 
-    if not flag:
-        break
-    else:
-        result += 1
-
+    result += 1
 print(result)
